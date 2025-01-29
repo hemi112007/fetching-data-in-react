b45ef7551ad08179712f619f125a6f43d4e33310
hamdan.ga <hamdanchitari@gmail.com>
2025-01-29 19:49:45 +0300

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
-import { useState } from 'react'
 // src/App.jsx
+import { useState , useEffect} from 'react'
+import './App.css'
+import * as studentService from './services/studentServices'
+
 
 const App = () => {
 
+  const [students, setStudents] = useState ([])
+  const [refreshTrigger, serRefreshTrigger] = useState(true)
+
+  const fetchAllStudents = async () => {
+    const studentData = await studentService.index()
+    // console.log(studentData)
+    setStudents(studentData)
+  }
+
+  
+  useEffect (() => {
+    fetchAllStudents ()
+  }, [refreshTrigger])
+
+const handledelete = async(id) =>{
+  await studentService.deleteStudent(id)
+setRefreshTrigger(!refreshTrigger)
+}
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Fetch All the Students</h1>
+      <ul>
+        {students.map ((student) => (
+          <li key ={student._id} > {student.name}
+          <button onClick ={() => handleDelete(student._id)}>X</button>
+           </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
 export default App
+
+
+
+
+
+
+
 
